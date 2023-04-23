d8f64c061aee37e4d3d809808b692f4b28ec6687
Okpainmo <okpainmoandrew@gmail.com>
2023-04-23 18:55:52 +0100

    refactor: updated tailwind xsm end-point

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       screens: {
-        xsm: '450px',
+        xsm: '350px',
         // => @media (min-width: 450px) { ... }
 
         sm: '640px',
