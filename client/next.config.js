8841514e8f45b0324ab24e0795db562486e58157
Okpainmo <okpainmoandrew@gmail.com>
2023-04-23 18:55:16 +0100

    refactor: whitelisted AWS image retrieval url

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ['journie-users-profile-image-bucket.s3.eu-north-1.amazonaws.com'],
+  },
+};
